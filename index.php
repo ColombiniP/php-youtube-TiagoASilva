636b526e64b550dcc813eb36b949058dadbcbb56
Paschoal Colombini <paschoal.colombini@live.com>
2024-09-03 14:36:15 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index - PHP</title>
+    <style>
+        nav header h1 {
+            text-align: center;
+        }
+
+        nav ul {
+            display: flex;
+            flex-direction: row;
+            gap: 2rem;
+            list-style: none;
+            justify-content: center;
+            text-transform: uppercase;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <nav>
+        <header>
+            <h1>
+                PHP - Aprendendo
+            </h1>
+        </header>
+        <ul>
+            <li>Home</li>
+            <li>Variáveis</li>
+            <li>Funções</li>
+            <li>Arrays</li>
+        </ul>
+        <hr>
+    </nav>
+</body>
+</html>
