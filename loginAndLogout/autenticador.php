b1fa3e1629254d18fe7a968c23249a49960d9f1b
Paschoal Colombini <paschoal.colombini@live.com>
2024-09-28 15:06:25 -0300

    fizemos página index logada, e opção para deslogar
    vimos unset que destroi a sessao aberta, isset que verifca se uma variável é definida, header('Location: ') que encaminha para uma pagina

--- loginAndLogout/autenticador.php
+++ loginAndLogout/autenticador.php
@@ -1,9 +1,28 @@
 <?php
+session_start();
 
 try {
     $usuario = "paschoal"; 
     $password = "1234";
-    
-} catch (Throwable $e) {
+
+    $usuario_digitado = $_POST["usuario"];
+    $password_digitado = $_POST["password"];
+
+
+    if($usuario_digitado === $usuario) {
+        if ($password_digitado === $password) {
+            $_SESSION["usuario_logado"] = $usuario_digitado;
+            header("Location: index.php?logado=true");
+        } else {
+            header("Location: login.php?falhou=true");
+        }
+    } else {
+        header("Location: login.php?falhou=true");
+    }
+
+
+
+
+} catch (Exception $e) {
     echo $e->getMessage();
 }
--- loginAndLogout/index.php
+++ loginAndLogout/index.php
@@ -1,3 +1,46 @@
+<!DOCTYPE html>
 <?php
+session_start();
 
-require 'login.php';
+if (!isset($_SESSION["usuario_logado"])) {
+    header("Location: login.php");
+}
+
+if (isset($_GET["sair"])) {
+    unset($_SESSION["usuario_logado"]);
+    header("Location: login.php");
+}
+
+?>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Área logada</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1 style="text-align: center;">Bem-vindo a área restrita</h1>
+        </header>
+        <main>
+            <div class="saudacao">
+                <?php echo "Olá " . $_SESSION["usuario_logado"] . "!<br>"; ?>
+            </div>
+        </main>
+        <footer>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="index.php?sair=true" name="sair">Sair</a>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+    </div>
+</body>
+
+</html>
