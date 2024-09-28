b0f350e0c4b3b5b92888ecb9e75da8a7d6db02f5
Paschoal Colombini <paschoal.colombini@live.com>
2024-09-28 12:06:39 -0300

    melhorando o c´digo de processamento de upload
    adicionando tratamento para files .exe e informando cada tipo de erro, além de nomera unicamente cada arquivo que sobe para o servidor com um nome, data e hora e id aleatória

--- index.php
+++ index.php
@@ -1,39 +1,11 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index - PHP</title>
-    <style>
-        nav header h1 {
-            text-align: center;
-        }
-
-        nav ul {
-            display: flex;
-            flex-direction: row;
-            gap: 2rem;
-            list-style: none;
-            justify-content: center;
-            text-transform: uppercase;
-            font-weight: bold;
-        }
-    </style>
+    <title>Document</title>
 </head>
 <body>
-    <nav>
-        <header>
-            <h1>
-                PHP - Aprendendo
-            </h1>
-        </header>
-        <ul>
-            <li>Home</li>
-            <li>Variáveis</li>
-            <li>Funções</li>
-            <li>Arrays</li>
-        </ul>
-        <hr>
-    </nav>
+    
 </body>
 </html>
--- upload-simples/up-simples.php
+++ upload-simples/up-simples.php
@@ -1,30 +1,72 @@
 <?php
+// date_default_timezone_get('America/Sao_Paulo');
 
 try {
-    
-    var_dump($_FILES)."<br>";
-    $nameFile = $_FILES['arquivo-up']['name'];
-    $tmpName = $_FILES['arquivo-up']['tmp_name'];
-    var_dump($nameFile)."<br>";
-    var_dump($tmpName)."<br>";
 
     $diretorioDestinoUpload = 'enviados/';
-    if (is_dir($diretorioDestinoUpload)) {
-        var_dump($diretorioDestinoUpload)."<br>";
-        echo "É um diretório válido<br>";
-    } else {
+
+    // verificando se diretório é válido
+    if (!is_dir($diretorioDestinoUpload)) {
         throw new Exception("Diretório inexistente!");
     }
 
-    $nome_arquivo = $diretorioDestinoUpload . basename($nameFile);
-    var_dump($nome_arquivo)."<br>";
+    // verificando se arquivo é um .exe
+    if(is_executable($_FILES['arquivo-up']['tmp_name'])) {
+        throw new Exception("Arquivos executáveis não permitido.");
+    }
 
-    if(!move_uploaded_file($tmpName,$nome_arquivo)) {
-        throw new Exception("Não foi possível fazer o upload do arquivo!");
+    // obtemos as extenção do arquivo
+    $exe_extensao = pathinfo($_FILES['arquivo-up']['name'],PATHINFO_EXTENSION);
+
+    // criamos um nome único para o arquivo
+    $nome_unico = uniqid("recebido_".date("d.m.y_H:i")."_").".".$exe_extensao;
+        // recebido_28.09.24_14:24_66f8119b1f447.php
+     
+    // enviando para o servidor
+    $nome_arquivo_servidor = $diretorioDestinoUpload . $nome_unico;
+
+    // pegando o arquivo tmp
+    $tmpName = $_FILES['arquivo-up']['tmp_name'];
+
+    if(!move_uploaded_file($tmpName,$nome_arquivo_servidor)) {
+        // tratendo os códigos de erros em file['file']['error']
+        switch($_FILES['arquivo-up']['error']) {
+            case 1 :
+                echo "Error : " . $_FILES['arquivo-up']['error']."<br>";
+                throw new Exception("O arquivo enviado excede o limite definido na diretiva upload_max_filesize do php.ini.");
+                break;
+            case 2 :
+                echo "Error : " . $_FILES['arquivo-up']['error']."<br>";
+                throw new Exception("O arquivo excede o limite definido em MAX_FILE_SIZE no formulário HTML.");
+                break;
+            case 3 :
+                echo "Error : " . $_FILES['arquivo-up']['error']."<br>";
+                throw new Exception("O upload do arquivo foi feito parcialmente.");
+                break;
+            case 4 :
+                echo "Error : " . $_FILES['arquivo-up']['error']."<br>";
+                throw new Exception("Nenhum arquivo foi enviado.");
+                break;           
+            case 6 :
+                echo "Error : " . $_FILES['arquivo-up']['error']."<br>";
+                throw new Exception("Pasta temporária ausente.");
+                break;        
+            case 7 :
+                echo "Error : " . $_FILES['arquivo-up']['error']."<br>";
+                throw new Exception("Falha ao escrever o arquivo no disco.");
+                break;    
+            case 8 :
+                echo "Error : " . $_FILES['arquivo-up']['error']."<br>";
+                throw new 
+                Exception("Uma extensão do PHP interrompeu o upload do arquivo. ");
+                break;
+            default :
+                echo "Error : " . $_FILES['arquivo-up']['error']."<br>";
+                throw new Exception("Não foi possível fazer o upload do arquivo!");    
+        }
     } else {
         echo "Arquivo salvo na pasta";
     }
-
 
 } catch (Throwable $e) {
     echo $e->getMessage();
