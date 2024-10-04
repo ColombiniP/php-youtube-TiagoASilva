75e54e68fae516c220e35137fcb7578f54454016
Paschoal Colombini <paschoal.colombini@live.com>
2024-10-04 11:15:16 -0300

    rotas das pastas em localhost index

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    
+    <a href="./conecta-mysql/">conecta-mysql</a><br>
+    <a href="./formularios/formulario-html.php">formularios</a><br>
+    <a href="./classes_objetos_metodos_construtores/">classes_objetos_metodos_construtores</a><br>
+    <a href="./arrays_objetos/">arrays de objetos</a><br>
+    <a href="./arrays_while_foreach/">arrays while e for</a><br>
+    <a href="./arrays/">arrays </a><br>
+    <a href="./loginAndLogout/">Login e logout</a><br>    
+    <a href="./upload-simples/">upload simples de arquivos</a><br>    
 </body>
 </html>
